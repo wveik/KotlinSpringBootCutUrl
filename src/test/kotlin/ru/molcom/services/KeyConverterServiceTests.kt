3812d67123ea45d38463e79e36d862ad051611fd
wveik <kataev_06@list.ru>
2019-11-25 14:13:12 +0300

    add tests KeyConverterServiceTests

---
+++ src/test/kotlin/ru/molcom/services/KeyConverterServiceTests.kt
@@ -0,0 +1,45 @@
+package ru.molcom.services
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.test.context.junit4.SpringRunner
+import ru.molcom.services.impl.DefaultKeyConverterService
+
+
+@RunWith(SpringRunner::class)
+class KeyConverterServiceTests {
+
+    @TestConfiguration
+    internal class EmployeeServiceImplTestContextConfiguration {
+        @Bean
+        fun employeeService(): KeyConverterService {
+            return DefaultKeyConverterService()
+        }
+    }
+
+    @Autowired
+    private val service: KeyConverterService? = null
+
+    private val TST_ID: Long = 3000001L
+    private val TST_KEY: String = "aaaA"
+
+    @Test
+    fun keyToIdTest() {
+        var key = service?.idToKey(TST_ID)
+        var id = key?.let { service?.keyToId(it) }
+
+        Assert.assertEquals(id, TST_ID)
+    }
+
+    @Test
+    fun idToKeyTest() {
+        var id = service?.keyToId(TST_KEY)
+        var key = id?.let { service?.idToKey(it) }
+
+        Assert.assertEquals(key, TST_KEY)
+    }
+}
